97d1b4eabc23369152c15985563ee56cfb146378
Ishrath <ishrathbegam007@gmail.com>
2026-09-02 02:02:36 +0530

    Added new program

---
+++ controstatement/Els.java
@@ -0,0 +1,18 @@
+class Els {
+    public static void main(String[] args) {
+        int mark = 75;
+
+if (mark >= 90) {
+    System.out.println("A Grade");
+}
+else if (mark >= 75) {
+    System.out.println("B Grade");
+}
+else if (mark >= 50) {
+    System.out.println("C Grade");
+}
+else {
+    System.out.println("Fail");
+}
+    }
+}
---
+++ controstatement/Main.java
@@ -0,0 +1,12 @@
+class Main{
+    public static void main(String[] args) {
+
+        int age = 07;
+
+if (age >= 18) {
+    System.out.println("Eligible");
+} else {
+    System.out.println("Not Eligible");
+}
+    }
+}
