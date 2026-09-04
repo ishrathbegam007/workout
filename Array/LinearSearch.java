36d3ad23bb180f8898e03a2d3f75c58d4ad8fd00
Ishrath <ishrathbegam007@gmail.com>
2026-09-04 23:34:50 +0530

    Added Array statement

---
+++ Array/LinearSearch.java
@@ -0,0 +1,23 @@
+class LinearSearch {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+        int target = 80;
+
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == target) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Element found");
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
--- Array/Reversed.java
+++ Array/Reversed.java
@@ -1,4 +1,4 @@
-public class Reversed {
+class Reversed {
     public static void main(String[] args) {
 
         int[] arr = {10, 20, 30, 40, 50};
